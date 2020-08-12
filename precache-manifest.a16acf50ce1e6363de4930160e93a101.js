4fdf465c93c465b74a8bb32840d9bc0a0849d3db
Иван Минаков <hardwit@iMac-Ivan.local>
2020-08-12 20:54:56 +0300

    Updates

---
+++ precache-manifest.a16acf50ce1e6363de4930160e93a101.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786e3c3b4c61460bf7267d8af2fa060f",
+    "url": "/vk-flappy-bird/index.html"
+  },
+  {
+    "revision": "3ae61eb6d9f4600d516a",
+    "url": "/vk-flappy-bird/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "3ae61eb6d9f4600d516a",
+    "url": "/vk-flappy-bird/static/js/2.c0f66518.chunk.js"
+  },
+  {
+    "revision": "e9eb83b6a2290cb6aaaf889dc89cb319",
+    "url": "/vk-flappy-bird/static/js/2.c0f66518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687b3ee4823b21118eba",
+    "url": "/vk-flappy-bird/static/js/main.1bd43f76.chunk.js"
+  },
+  {
+    "revision": "79a28816348cc649c94b",
+    "url": "/vk-flappy-bird/static/js/runtime-main.374539dc.js"
+  },
+  {
+    "revision": "671bb8b65d3da2fb167f76f343529013",
+    "url": "/vk-flappy-bird/static/media/pipe-green-bottom.671bb8b6.png"
+  }
+]);
